77df2e1e16b5c72e0858b4659a457786cff32a96
vaibhav111tandon <vaibhav.tandongcet@gmail.com>
2019-02-13 16:19:26 +0530

    Added UUIDV4

--- index.js
+++ index.js
@@ -165,6 +165,14 @@ module.exports = {
             }
             return a;
         }
+    },
+    isUUIDv4: function(uuid){
+        var re = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}/;
+        if(re.test(uuid)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
